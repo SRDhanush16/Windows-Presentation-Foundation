aec591bc570713fa670b400be661527c4078d09e
Dhanush S R <141258864+SRDhanush16@users.noreply.github.com>
2024-02-04 15:32:30 +0530

    Add files via upload

---
+++ Monument_recognizer/Monument_recognizer--main/LandmarkAI_WPF/LandmarkAI_WPF/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using LandmarkAI_WPF.Classes;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LandmarkAI_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialogs = new OpenFileDialog();
+            dialogs.Filter = "Image files (*.png;*.jpg)|*.png;*.jpg;*.jpeg|All files(*.*)|*.*";
+            dialogs.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (dialogs.ShowDialog() == true)
+            {
+                String filename = dialogs.FileName;
+                selectedImage.Source = new BitmapImage(new Uri(filename));
+                MakePredictionAsyn(filename);
+            }
+
+        }
+
+        private async void MakePredictionAsyn(string filename)
+        {
+            string url = "https://centralindia.api.cognitive.microsoft.com/customvision/v3.0/Prediction/994f9cfc-967b-4744-ab40-56a23bc0440e/classify/iterations/Iteration1/image";
+            string prediction_key = "8f526ece46f4426da07430fa934ada4d";
+            string content_type = "application/octet-stream";
+
+            var file = File.ReadAllBytes(filename);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", prediction_key);
+                using (var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(content_type);
+                    var response = await client.PostAsync(url, content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    List<Prediction> predictions = new List<Prediction>((JsonConvert.DeserializeObject<CustomVision>(responseString)).Predictions);
+                    predictionsListView.ItemsSource = predictions;
+
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Monument_recognizer/Monument_recognizer--main/README.md
@@ -0,0 +1,31 @@
+# Monument_recognizer-
+developed a monument recognizer using Azure CustomVision and .NET Framework WPF
+
+## Overview
+* IDE :                 Visual Studio \
+* Framework :           .NET Framework \
+* Application-Type :    WPF (Windows Presentation Foundation)\
+* Service :             Cognitive Service -> Image Recongnition \
+* Cloud Platform :      Azure \
+* Cloud service used:   Azure-CustomVision (https://www.customvision.ai/) \
+
+## Training the CustomVision AI
+
+  ![image](https://github.com/SRDhanush16/Monument_recognizer-/assets/141258864/9c4da996-2320-44ee-a16b-43e95f314e48)
+
+## Application:
+
+### Golden Gate ###
+
+  ![image](https://github.com/SRDhanush16/Monument_recognizer-/assets/141258864/76986534-3173-4a17-a5ae-5332bc4de825)
+
+### Eiffel Tower ###
+
+  ![image](https://github.com/SRDhanush16/Monument_recognizer-/assets/141258864/7d49b5db-53be-44c0-a61a-804150874d6b)
+
+### Some Random Photo  ###
+
+  ![image](https://github.com/SRDhanush16/Monument_recognizer-/assets/141258864/9edfc1b5-2e80-4bc2-aa58-b1671dc886cb)
+
+
+
